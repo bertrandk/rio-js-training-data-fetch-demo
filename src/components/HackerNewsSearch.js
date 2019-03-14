4c90f623ab01f6742ffa1080512652909fe61881
Evan Schultz <evan@rangle.io>
2019-03-13 23:04:25 -0400

    basic api solution

--- src/components/HackerNewsSearch.js
+++ src/components/HackerNewsSearch.js
@@ -5,11 +5,31 @@ const API = 'https://hn.algolia.com/api/v1/search?query=react';
 class HackerNewsSearch extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      results: []
+    };
   }
-  componentDidMount() {}
+  componentDidMount() {
+    fetch(API)
+      .then(result => result.json())
+      .then(({ hits }) => {
+        this.setState({ results: hits });
+      });
+  }
 
   render() {
-    return <section>Waiting for results</section>;
+    let { results } = this.state;
+    return (
+      <ul>
+        {results.map(result => {
+          return (
+            <li key={result.objectID}>
+              <a href={result.url}>{result.title}</a>
+            </li>
+          );
+        })}
+      </ul>
+    );
   }
 }
 
